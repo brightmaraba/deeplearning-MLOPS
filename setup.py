1900cfe70b774151bbb3f8f237e84df183b6e038
brightmaraba <brightkoech@gmail.com>
2024-04-16 20:01:14 +0800

    Requirements added and installed

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "deeplearning-MLOPS"
+AUTHOR_USER_NAME = "brightmaraba"
+AUTHOR_EMAIL = "brightkoech@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A CNN classifier for classifying Kidney Disease",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+)
